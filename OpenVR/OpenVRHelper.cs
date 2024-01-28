696906d0fe560d116fc4dd5fb67be75c86fdd389
Nicolas Gnyra <nicolas@gnyra.com>
2024-01-28 14:49:25 -0500

    Initial commit

---
+++ OpenVR/OpenVRHelper.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Valve.VR
+{
+    /// <summary>
+    /// Helper for OpenVR/SteamVR-related operations.
+    /// </summary>
+    public class OpenVRHelper
+    {
+        /// <summary>
+        /// Initialize OpenVR as an overlay. This allows direct communication with SteamVR while maintaining the active OpenXR session. This method is idempotent.
+        /// </summary>
+        /// <returns>Whether or not initialization was successful.</returns>
+        public static bool Initialize()
+        {
+            if (OpenVR.System != null)
+            {
+                return true;
+            }
+
+            Debug.Log("Initializing OpenVR in Overlay mode");
+
+            EVRInitError error = EVRInitError.None;
+            CVRSystem system = OpenVR.Init(ref error, EVRApplicationType.VRApplication_Overlay);
+
+            if (error != EVRInitError.None)
+            {
+                Debug.LogError("Failed to initialize OpenVR in Overlay mode: " + error);
+                return false;
+            }
+
+            if (system == null)
+            {
+                Debug.LogError("Failed to initialize OpenVR in Overlay mode: System is null");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Beat Saber OpenVR API
+[![GitHub Actions Workflow Status](https://img.shields.io/github/actions/workflow/status/nicoco007/BeatSaber-OpenVR-API/build.yml?style=flat-square)](https://github.com/nicoco007/BeatSaber-OpenVR-API/actions/workflows/build.yml)
+[![Latest Release](https://img.shields.io/github/v/release/nicoco007/BeatSaber-OpenVR-API?style=flat-square)](https://github.com/nicoco007/BeatSaber-OpenVR-API/releases/latest)
+
+A library for Beat Saber that provides the API bindings to communicate with the OpenVR runtime (SteamVR). For more details on the API, samples, and documentation, visit [ValveSoftware/openvr](https://github.com/ValveSoftware/openvr).
